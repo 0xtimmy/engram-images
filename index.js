2c29c5c233aae6078f983e6b51e0755d577d335c
Timmy <>
2023-02-21 17:35:10 -0500

    image delete works

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ const API_TOKEN = ""
 
 export const corsHeaders = {
   "Access-Control-Allow-Headers": "*",
-  "Access-Control-Allow-Methods": "GET POST DELETE",
+  "Access-Control-Allow-Methods": "GET, DELETE",
   "Access-Control-Allow-Origin": "*",
 };
 
 async function handleRequest(request) {
   if (request.method === "OPTIONS") {
-    return new Respose("OK", {
+    return new Response("OK", {
       headers: corsHeaders,
     });
   } else if (request.method === "GET") {
@@ -33,12 +33,12 @@ async function handleRequest(request) {
     const result = await response.text();
     return new Response(result, { status: 200, headers: corsHeaders });
   } else if (request.method == "DELETE") {
-    const response = await fetch(`https://api.cloudflare.com/client/v4/accounts/${ACCOUNT_ID}/images/v1/${request.query.imgId}`, {
+    const response = await fetch(`https://api.cloudflare.com/client/v4/accounts/${ACCOUNT_ID}/images/v1/${request.body}`, {
       method: "DELETE",
       headers: {
         ...corsHeaders,
         "Authorization": `Bearer ${API_TOKEN}`,
-        "X-Auth-Email": "timmy0x@proton.me"
+        "X-Auth-Email": ""
       }
     })
     const result = await response.text();
